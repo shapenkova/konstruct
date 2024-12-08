a3ea4b3a732762ddecf33416b39e45ef42214f67
Anastasia Shapenkova <wondernasty@yandex.ru>
2024-12-08 17:08:40 +0500

    сделала пару блоков и адаптив

--- src/main.js
+++ src/main.js
@@ -8,12 +8,16 @@ import header from './html/page-header.html';
 import footer from './html/page-footer.html';
 import poster1 from './html/first-poster.html';
 import story from './html/story.html'
+import capabilities from './html/capabilities.html';
+import want from './html/want.html';
 
 const app = document.getElementById('app');
 app.innerHTML = `   
     ${poster1}
     ${header} 
     ${story}
+    ${capabilities}
+    ${want}
     ${footer}`;
 
    //навигационное меню
@@ -21,4 +25,14 @@ const root = document.documentElement;
 const navToggle = document.querySelector("#js-navToggle");
 navToggle.addEventListener("click", function () {
     root.classList.toggle("show-nav");
+});
+
+document.querySelector('.first-poster__link-scroll').addEventListener('click', function(event) {
+    event.preventDefault(); 
+    const targetId = this.getAttribute('href'); 
+    const targetElement = document.querySelector(targetId); 
+
+    targetElement.scrollIntoView({
+        behavior: 'smooth' 
+    });
 });
